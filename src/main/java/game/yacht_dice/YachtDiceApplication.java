5f17ccec9e05242d64d593008e9fc0eafbf6d8bf
KIMSEI1124 <workju1124@gmail.com>
2022-11-28 17:07:26 +0900

    init: yacht_dice

---
+++ src/main/java/game/yacht_dice/YachtDiceApplication.java
@@ -0,0 +1,7 @@
+package game.yacht_dice;
+
+public class YachtDiceApplication {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
---
+++ src/test/java/game/yacht_dice/YachtDiceApplicationTests.java
@@ -0,0 +1,10 @@
+package game.yacht_dice;
+
+import org.junit.Test;
+
+class YachtDiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
