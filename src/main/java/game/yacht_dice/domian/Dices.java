8e972b80c388b9339b92bad793ad7207e7073650
KIMSEI1124 <workju1124@gmail.com>
2022-12-01 19:22:05 +0900

    feat: Dices Create
    도메인 계층에서 Dices 생성
    reRoll 기능으로 원하는 주사위를 새로 돌릴수 있음

---
+++ src/main/java/game/yacht_dice/domian/Dices.java
@@ -0,0 +1,35 @@
+package game.yacht_dice.domian;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+
+public class Dices {
+    private final List<Integer> numbers;
+    Random random = new Random();
+
+    public Dices() {
+        this.numbers = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            int number = random.nextInt(6);
+            this.numbers.add(number);
+            dicesSort();
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public void reRoll(int... index) {
+        for (int i : index) {
+            int number = random.nextInt(6);
+            this.numbers.set(i, number);
+        }
+    }
+
+    private void dicesSort() {
+        this.numbers.sort(Comparator.naturalOrder());
+    }
+}
